952c781674451cde47a0e8768f5f15cd4adcbf45
TeamLeadTL <bissb163@gmail.com>
2024-11-21 09:48:44 +0530

    modefied with grid

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import Form3 from "./components/Form3.js";
 import Form4 from "./components/Form4.js";
 function App() {
   return (
-    <React.Fragment>
-    <div >
-      <div >
-        <Form2 />
-      </div>
-      <div>
-        <Form3 />
-      </div>
-      <div>
-        <Form4 />
+  //   <React.Fragment>
+  //   <div >
+  //     <div >
+  //       <Form2 />
+  //     </div>
+  //     <div>
+  //       <Form3 />
+  //     </div>
+  //     <div>
+  //       <Form4 />
+  //     </div>
+  //   </div>
+  // </React.Fragment>
+  <React.Fragment>
+    <div style={{border:'2px solid gray'}} className="container">
+      <div className="row">
+        <div style={{border:'2px solid green'}} className="col-sm-12"><Form2 /></div>
+        <div style={{border:'2px solid green'}} className="col-sm-12"><Form3 /></div>
+        <div style={{border:'2px solid green'}} className="col-sm-12"><Form4 /></div>    
       </div>
     </div>
   </React.Fragment>
